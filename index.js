3f216799bfbd412ca209c3f191d79eb633864b80
root <saung.myat@gmail.com>
2022-03-06 22:32:34 -0500

    commit tree 930731c22dd8777fef46f00a4a22b1f4b7263a83

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//const titleCased = () => {
+  //return tutorials
+//}
+let returnArr = [];
+function titleCased() {
+  for( const name of tutorials) {
+    let newArr = name.split(' ');
+    let sentence = [];
+    let string = '';
+    for(const words of newArr) {
+      let letter = words.charAt(0).toUpperCase() + words.slice(1);
+      sentence.push(letter);
+      string = sentence.join(' ');
+    }
+    returnArr.push(string);
+    /*console.log(newArr);
+    newArr[0] = newArr[0].charAt(0).toUpperCase() + newArr[0].slice(1);
+    console.log(newArr);
+    newArr = newArr.join(' ');
+    console.log(newArr);*/
+    //returnArr.push(newArr);
+ 
+    
+    
+  }
+  return returnArr;
 }
+
+
